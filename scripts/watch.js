4b0be479ec8e1d856b95b4f86bdb0812629f30a6
JimSchofield <oldcoyote@gmail.com>
2020-09-26 14:04:11 -0500

    Added cleanup function for watch to close chokidar and http server on exit.

--- scripts/watch.js
+++ scripts/watch.js
@@ -3,7 +3,7 @@
 const path = require("path");
 const fs = require("fs");
 const shell = require("shelljs");
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
 const chokidar = require("chokidar");
 
 const config = JSON.parse(
@@ -24,22 +24,23 @@ console.log("| Watching files        |");
 console.log("+-----------------------+");
 console.log("");
 
-const templatesPath = path.resolve(process.cwd(), config.paths.templates ? config.paths.templates : 'templates');
+const templatesPath = path.resolve(
+  process.cwd(),
+  config.paths.templates ? config.paths.templates : "templates"
+);
 console.log(templatesPath);
 console.log(fs.existsSync(templatesPath) ? "Does exist" : "doesn't exist");
 const watcher = chokidar
   .watch([
     path.resolve(process.cwd(), config.paths.sourceDir),
-    ...(fs.existsSync(templatesPath) ? [templatesPath] : [])
+    ...(fs.existsSync(templatesPath) ? [templatesPath] : []),
   ])
-  .on('change', (event, path) => {
+  .on("change", (event, path) => {
     console.log("Change detected... re-building");
     shell.exec("blogli");
     console.log("Done!");
   });
-
 console.log(watcher.getWatched());
-
 
 console.log("");
 console.log("+-----------------------+");
@@ -49,10 +50,21 @@ console.log("");
 
 const servePath = config.paths.targetDir;
 const httpServer = spawn("http-server", [servePath]);
-httpServer.stdout.on('data', (data) => {
+httpServer.stdout.on("data", (data) => {
   console.log(data.toString());
 });
 
-httpServer.stderr.on('data', (data) => {
+httpServer.stderr.on("data", (data) => {
   console.log(data.toString());
 });
+
+// Cleanup
+function onExitCleanup(_exitCode) {
+  httpServer.kill();
+  watcher.close()
+}
+process.on("exit", onExitCleanup);
+process.on("SIGINT", onExitCleanup);
+process.on("SIGUSR1", onExitCleanup);
+process.on("SIGUSR2", onExitCleanup);
+process.on("uncaughtException", onExitCleanup);
