33054bbf3bdb255d4e3ee23f56c1a40281d28df6
JimSchofield <oldcoyote@gmail.com>
2020-09-26 14:18:35 -0500

    Giving more helpful warning if languages are not given in blogli.json

--- src/initPrism.ts
+++ src/initPrism.ts
@@ -2,10 +2,18 @@ import Prism from "prismjs";
 import { Config } from "./types/config";
 
 export default async (config: Config): Promise<typeof Prism> => {
-  if (config.prismjs) {
+  if (
+    config.prismjs &&
+    config.prismjs.languages &&
+    config.prismjs.languages.length > 0
+  ) {
     config.prismjs.languages.forEach(async (lang) => {
       await import(`../node_modules/prismjs/components/prism-${lang}`);
     });
+  } else {
+    throw new Error(
+      "No languages provided in blogli.js: prismjs.languages should be an array of language name strings"
+    );
   }
   return Prism;
 };
